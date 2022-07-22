5c46eda2360689def1c5681b33f5970415df21e0
Romulo Brum da Silva <102628558+RomuloBrumDev@users.noreply.github.com>
2022-07-22 11:05:29 -0300

    atualização e acerto de erros

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
-/ Crie uma class component 
-
-
-// Guarde em um state as seguintes propriedades: nome, idade, comida favorita, e uma array com pelo menos 3 músicas.
-
-// Renderize na tela:
-// Seu nome em um h1
-// Sua idade em um h2
-// Sua comida favorita em um h3
-// Suas músicas favoritas em uma lista (ul / ol)
-
-
-**Bônus
-// Adicione uma imagem da sua fruta favorita via import
-
-import React, {Component} from 'React';
+import React, { Component } from "react";
 
 class myInfo extends Component {
-
-    nome:'Rômulo Brum',
-    idade: 42;
-    comida favorita: 'Comida Japonesa'
-    músicas favoritas: ['One More Time - Daft Punk', 'A Lenda - Sandy & Júnior', 'Pop - N*SYNC']
-
-  render () {
-    return(
-        <div> 
-          <h1>Rômulo Brum</h1>
-          <h2>43</h2>
-          <h3>Comida Japonesa - Todas</h3>
-          <ul>
-            <li>One More Time - Daft Punk</li>
-            <li>A Lenda - Sandy & Júnior</li>
-            <li>Pop - N*Sync</li>
-          </ul>
-        </div>
+  state = {
+    nome: "Rômulo Brum",
+    idade: 42,
+    comidaFavorita: "Comida Japonesa",
+    musicasFavoritas: [
+      "One More Time - Daft Punk",
+      "A Lenda - Sandy & Júnior",
+      "Pop - N*SYNC"
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comidaFavorita}</h3>
+        <ul>
+          <li>{this.state.musicasFavoritas[0]}</li>
+          <li>{this.state.musicasFavoritas[1]}</li>
+          <li>{this.state.musicasFavoritas[2]}</li>
+        </ul>
+      </div>
     );
-  }  
+  }
 }
 
 export default myInfo;
